55bb7700281910c72eadbfc472724981de8a6ed2
Anvict1 <99268928+Anvict1@users.noreply.github.com>
2022-05-26 06:48:30 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
+package com.company;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+      for(int i = 500; i <= 650; i = i + 10) {
+       System.out.print(i + " ");
+      }
+
+       int i = 500;
+       while (i <= 650) {
+           i = i + 10;
+          System.out.println(i + " ");
+
+       }
+
+
+
+       int b = 500;
+        do {
+           System.out.println(i);
+          b = b + 10;
+        } while (b <= 650);
+   }
 }
